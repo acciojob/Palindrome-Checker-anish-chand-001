8203aefca7483f1351bba6a28d752204c1b98c4c
Anish Chand <anishchand.data@gmail.com>
2026-06-29 22:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,19 @@
 
 function palindrome(str){
 
+	let string  = str.toLowerCase()
+	let left = 0
+	let right = string.length-1
+
+	while(left < right){
+
+		if(string[left] !== string[right] ){
+			return flase
+		}
+		left++
+		right--
+	}
+	return true
+
 }
 module.exports = palindrome
